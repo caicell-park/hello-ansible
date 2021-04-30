340221cdb8c178e5e4e5c5202eabb005a5473c11
su.park <su.park@kakaocorp.com>
2021-04-30 14:49:59 +0900

    Pull an EC2 with a tag, Project:Value to inventory

--- scripts/get-all-ec2.sh
+++ scripts/get-all-ec2.sh
@@ -4,3 +4,10 @@ echo "[all]" > inventory
 aws ec2 describe-instances \
    --query 'Reservations[*].Instances[*].PublicIpAddress' \
    --output text >> inventory
+
+echo "" >> inventory
+echo "[udacity]" >> inventory
+aws ec2 describe-instances \
+   --query 'Reservations[*].Instances[*].PublicIpAddress' \
+   --filters "Name=tag:Project,Values=udacity" \
+   --output text >> inventory
